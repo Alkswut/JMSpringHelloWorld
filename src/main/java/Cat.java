e607a44896280b58ee9b5bdd5ed6844b6fd23674
AleksandrVoroninSME <aleksandr_av@bk.ru>
2022-01-05 00:59:55 +0300

    Task2_1_2 (UPD_Finish)

---
+++ src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private String name;
+
+    public String getMessage() {
+        return name;
+    }
+
+    public void setMessage(String message) {
+        this.name = message;
+    }
+}
